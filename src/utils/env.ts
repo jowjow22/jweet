fa436104424bbd1fd8926adae4b7dd19d70714e4
jonathan santos da silva <jonathan224santos@gmail.com>
2024-09-21 11:23:24 -0300

    fix: update secret config

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -6,7 +6,7 @@ loadEnvConfig(projectDir)
 
 const envSchema = z.object({
   BCRYPT_SALT_ROUND: z.coerce.number().default(10),
-  JWT_SECRET: z.string(),
+  NEXTAUTH_SECRET: z.string(),
   GOOGLE_CLIENT_ID: z.string(),
   GOOGLE_CLIENT_SECRET: z.string(),
 })
